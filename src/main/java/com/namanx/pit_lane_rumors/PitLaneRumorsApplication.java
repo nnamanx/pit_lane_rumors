639c6fee0853d87031ea95c9eea472c78969cb14
Laman Khudadatzada <lkhudadatzada@gmail.com>
2024-03-07 15:38:26 +0400

    "src package config"

---
+++ src/main/java/com/namanx/pit_lane_rumors/PitLaneRumorsApplication.java
@@ -0,0 +1,13 @@
+package com.namanx.pit_lane_rumors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitLaneRumorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PitLaneRumorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/namanx/pit_lane_rumors/PitLaneRumorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namanx.pit_lane_rumors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitLaneRumorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
