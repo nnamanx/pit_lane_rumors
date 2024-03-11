ca433301054cad8c4db7f12bc2bc0b0e55565c27
Laman Khudadatzada <lkhudadatzada@gmail.com>
2024-03-11 18:06:46 +0400

    "User request and its Messages"

--- src/main/java/com/namanx/pit_lane_rumors/model/constant/Messages.java
+++ src/main/java/com/namanx/pit_lane_rumors/model/constant/Messages.java
@@ -1,4 +1,14 @@
 package com.namanx.pit_lane_rumors.model.constant;
 
 public class Messages {
+
+    public final static String BLANK_USERNAME = "No username";
+    public final static String BLANK_PASSWORD = "No password";
+    public final static String USERNAME_REGEX = "\n" +
+            "Username can consist of lowercase and capitals\n" +
+            "Username can consist of alphanumeric characters\n" +
+            "Username can consist of underscore and hyphens and spaces\n" +
+            "Cannot be two underscores, two hypens or two spaces in a row\n" +
+            "Cannot have a underscore, hypen or space at the start or end";
+    public final static String PASSWORD_REGEX = "Enter at least 8 characters. At least one uppercase letter, one number, and one special character.";
 }
--- src/main/java/com/namanx/pit_lane_rumors/model/dto/request/UserRequest.java
+++ src/main/java/com/namanx/pit_lane_rumors/model/dto/request/UserRequest.java
@@ -1,4 +1,34 @@
 package com.namanx.pit_lane_rumors.model.dto.request;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.validation.annotation.Validated;
+
+import static com.namanx.pit_lane_rumors.model.constant.Messages.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Validated
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserRequest {
+
+    @NotBlank(message = BLANK_USERNAME)
+    @Pattern(
+            regexp = "/^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]*$/",
+            message = USERNAME_REGEX
+    )
+    String username;
+
+    @NotBlank(message = BLANK_PASSWORD)
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = PASSWORD_REGEX
+    )
+    String password;
+
 }
